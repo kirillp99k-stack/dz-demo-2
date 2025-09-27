a8aa3a9226bff1d770bcff2ca95d6d55a886a303
kiria1 <kirillp99@yandex.ru>
2025-09-28 02:38:16 +0500

    change + var text

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
+
 import "fmt"
+
 func main() {
 	const usdInEuro = 0.8541
 	const usdInRub = 83.61
-	fmt.Print(usdInRub / usdInEuro)
+	var text string = "EUR/RUB = "
+	fmt.Print(text, usdInRub/usdInEuro)
 
 }
